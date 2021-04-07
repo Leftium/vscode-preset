53f72d47661bc9f23c1ad2de4836d392846920b8
Leftium <john@leftium.com>
2021-04-07 20:19:02 +0900

    Add actions to add launch.json file.

--- preset.ts
+++ preset.ts
@@ -1,4 +1,16 @@
 import { Preset } from 'apply';
 
 Preset.setName('vscode-preset');
-Preset.extract();
+Preset.extract('.vscode/launch.json').withDots().whenConflict('ask');
+Preset.extract('.gitignore').withDots().whenConflict('skip');
+
+Preset.edit('.gitignore').update((content, preset) => {
+    let result = content;
+
+    if (!result.includes('.vscode')) {
+        result += `\n.vscode`;
+    }
+
+    return result;
+});
+
